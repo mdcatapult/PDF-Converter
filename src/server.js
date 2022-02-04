ed3c08459662269f7515b7252a2f391a4cadeb07
Brad <brad.rollings@md.catapult.org.uk>
2022-02-04 15:30:19 +0000

    re-designed project structure and removing ftp logic

--- src/server.js
+++ src/server.js
@@ -2,19 +2,33 @@ const http = require("http");
 const request = require("request-promise-native");
 const fs = require("fs");
 const express = require("express");
-
 const app = express();
 const port = 8000;
+const pdf2base64 = require('pdf-to-base64');
 
 const outputFilename = `${process.cwd()}/test_download.pdf`;
-startFileServer(outputFilename);
+
+app.use(express.static('src'))
+
+app.get("/download-pdf", (req, res) => {
+
+    pdf2base64(outputFilename)
+            .then(
+                (response) => {
+                    console.log(response); //cGF0aC90by9maWxlLmpwZw==
+                }
+            )
+            .catch(
+                (error) => {
+                    console.log(error); //Exepection error....
+                }
+            )
+})
+
 
 app.get("/", (req, res) => {
-  const path = req.url.split("=")[1];
-  downloadPDF(path, outputFilename)
-    .then(() => {
-      var fileName = "/index.html";
-      fs.promises
+    var fileName = "/index.html";
+    fs.promises
         .readFile(__dirname + fileName)
         .then((contents) => {
           res.setHeader("Content-Type", "text/html");
@@ -25,49 +39,11 @@ app.get("/", (req, res) => {
           res.writeHead(500);
           res.end(err);
         });
-    })
-    .catch((e) => {
-      req.uri
-        ? console.log(e)
-        : console.log("warn: Invalid URI on URL:", req.url);
-    });
-});
+})
 
 app.listen(port, () => {
   console.log(`example app listening on port ${port}`);
 });
-
-function startFileServer(filePath) {
-  http
-    .createServer(function (request, response) {
-      fs.readFile(filePath, function (error, content) {
-        if (error) {
-          if (error.code == "ENOENT") {
-            fs.readFile("./404.html", function (error, content) {
-              response.writeHead(200, { "Content-Type": "application/pdf" });
-              response.end(content, "utf-8");
-            });
-          } else {
-            response.writeHead(500);
-            response.end(
-              "Sorry, check with the site admin for error: " +
-                error.code +
-                " ..\n"
-            );
-            response.end();
-          }
-        } else {
-          response.writeHead(200, {
-            "Content-Type": "application/pdf",
-            "Access-Control-Allow-Origin": "*",
-          });
-
-          response.end(content, "utf-8");
-        }
-      });
-    })
-    .listen(8125);
-}
 
 async function downloadPDF(pdfURL, outputFilename) {
   let pdfBuffer = await request.get({ uri: pdfURL, encoding: null });
